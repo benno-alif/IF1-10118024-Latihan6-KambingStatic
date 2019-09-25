b3ca28d2d032ad3509b855f700415eed87f9005d
BennoAlif <bennoalif41@gmail.com>
2019-09-25 15:49:47 +0700

    Initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,20 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Implementasi pada dua class
+    */
+
+//class Mamalia{
+//    public static int jumlahKambing;
+//}
+
+public class Main {
+    public static final String NAMA_KAMBING = "MIDUN";
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+}
---
+++ src/com/benno/Mamalia.java
@@ -0,0 +1,5 @@
+package com.benno;
+
+public class Mamalia{
+    public static int jumlahKambing;
+}
